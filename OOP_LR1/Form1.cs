5d664cc4919e026c15c2524d11337aeda3886ebb
soldatmt2@mail.ru <soldatmt2@mail.ru>
2020-11-11 15:34:09 +0300

    Добавьте файлы проекта.

---
+++ OOP_LR1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_LR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartOperation()
+        {
+            this.textBox1.Clear();
+            for(int i = 32; i < 127; i++)
+            {
+                this.textBox1.Text += (char)i;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.StartOperation();
+        }
+    }
+}
