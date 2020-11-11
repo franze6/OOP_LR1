ba17bc96eb10bce43a784cd38e6144b5ef616476
soldatmt2@mail.ru <soldatmt2@mail.ru>
2020-11-11 15:36:53 +0300

    LR_1-1

--- OOP_LR1/Form1.Designer.cs
+++ OOP_LR1/Form1.Designer.cs
@@ -77,7 +77,7 @@ namespace OOP_LR1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(817, 402);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
