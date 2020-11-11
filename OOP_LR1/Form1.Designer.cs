12c34438da8fdb97f6fb5aa41ecda2e01bef0b20
soldatmt2@mail.ru <soldatmt2@mail.ru>
2020-11-11 19:25:01 +0300

    1-10

--- OOP_LR1/Form1.Designer.cs
+++ OOP_LR1/Form1.Designer.cs
@@ -32,9 +32,6 @@ namespace OOP_LR1
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.Result = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,9 +40,9 @@ namespace OOP_LR1
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(333, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 24);
+            this.label1.Size = new System.Drawing.Size(144, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Задание №9";
+            this.label1.Text = "Задание №10";
             // 
             // button1
             // 
@@ -53,7 +50,7 @@ namespace OOP_LR1
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Проверка";
+            this.button1.Text = "Операция";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -67,41 +64,11 @@ namespace OOP_LR1
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(177, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(359, 26);
-            this.textBox1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(91, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Строка";
-            // 
-            // Result
-            // 
-            this.Result.AutoSize = true;
-            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Result.Location = new System.Drawing.Point(368, 189);
-            this.Result.Name = "Result";
-            this.Result.Size = new System.Drawing.Size(0, 24);
-            this.Result.TabIndex = 6;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(817, 402);
-            this.Controls.Add(this.Result);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -117,9 +84,6 @@ namespace OOP_LR1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label Result;
     }
 }
 
--- OOP_LR1/Form1.cs
+++ OOP_LR1/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Linq;
+using Microsoft.VisualBasic;
 using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OOP_LR1
 {
@@ -13,19 +14,57 @@ namespace OOP_LR1
 
         private void StartOperation()
         {
-            string value = this.textBox1.Text;
-            foreach (var (item,i) in value.Select((item, i)=>(item, i)))
+
+            double a = double.Parse(Interaction.InputBox("Введите a", "Ввод коэффициентов", "", 500, 500));
+            double b = double.Parse(Interaction.InputBox("Введите b", "Ввод коэффициентов", "", 500, 500));
+            double c = double.Parse(Interaction.InputBox("Введите c", "Ввод коэффициентов", "", 500, 500));
+            double x1, x2;
+
+            if (a == 0)
             {
-                if (!char.IsLetterOrDigit(item) && !char.IsWhiteSpace(item))
+                if (b == 0)
                 {
-                    this.Result.Text = "Неправильная строка";
-                    this.textBox1.Focus();
-                    this.textBox1.SelectionStart = i;
-                    this.textBox1.SelectionLength = 1;
+                    MessageBox.Show("Корней нет.");
                     return;
                 }
+                else
+                {
+                    x1 = -c / b;
+                    x2 = x1;
+                }
             }
-            this.Result.Text = "Правильная строка";
+            else
+            {
+                if (b == 0)
+                {
+                    if (-c / a < 0)
+                    {
+                        MessageBox.Show("Действительных корней нет.");
+                        return;
+                    }
+                    else
+                    {
+                        x1 = Math.Sqrt(-c / a);
+                        x2 = x1;
+                    }
+                }
+                else
+                {
+                    double D = b * b - 4 * a * c;
+                    if (D < 0)
+                    {
+                        MessageBox.Show("Действительных корней нет.");
+                        return;
+                    }
+                    else
+                    {
+                        x1 = ((-b + Math.Sqrt(D)) / (2 * a));
+                        x2 = -x1;
+                    }
+                }
+            }
+
+            MessageBox.Show($"Корни уравнения: х1={x1} x2={x2}");
         }
 
         private void button2_Click(object sender, EventArgs e)
